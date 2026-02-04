9929ef4716e04442b163c87b2f622963293f8203
Rifat <172rifat722@gmail.com>
2026-02-04 14:28:53 +0500

    php artisan migrate

--- database/migrations/2026_02_04_085648_create_products_table.php
+++ database/migrations/2026_02_04_085648_create_products_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->index();
+            $table->decimal('price', 8, 2);
+            $table->foreignId('category_id')->constrained();
+            $table->boolean('in_stock');
+            $table->decimal('rating', 3, 2);
+            $table->fullText('name')->language('russian');
             $table->timestamps();
         });
     }
